65e60078c5bd32eaa5b935e6a26eb3afd7fee307
Scott Davis <scott@davisworld.org>
2008-12-18 23:33:28 -0700

    added list by user

--- grails-app/controllers/EntryController.groovy
+++ grails-app/controllers/EntryController.groovy
@@ -1,3 +1,23 @@
 class EntryController {
-    def scaffold = Entry
+  def scaffold = Entry
+  
+  def list = {
+      if(!params.max) params.max = 10
+      flash.id = params.id
+      if(!params.id) params.id = "No User Supplied"
+
+      def author = User.findByLogin(params.id)
+      def entryList
+      def entryCount
+      if(author){
+        def query = { eq('author', author) }
+        entryList = Entry.createCriteria().list(params, query)        
+        entryCount = Entry.createCriteria().count(query)
+      }else{
+        entryList = Entry.list( params )
+        entryCount = Entry.count()
+      }
+      
+      [ entryInstanceList:entryList, entryCount:entryCount  ]
+  }  
 }
